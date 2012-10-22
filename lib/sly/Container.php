3f62dfbfeda406e37be238e574daa237439dffe3
Christoph <christoph@webvariants.de>
2012-10-22 03:21:41 +0200

    avoid endless recursion
    --HG--
    branch : di-container

--- lib/sly/Container.php
+++ lib/sly/Container.php
@@ -100,105 +100,105 @@ class sly_Container implements ArrayAccess, Countable {
 	 * @return int|null
 	 */
 	public function getCurrentArticleID() {
-		return $this['sly-current-article-id'];
+		return $this->get('sly-current-article-id');
 	}
 
 	/**
 	 * @return int|null
 	 */
 	public function getCurrentLanguageID() {
-		return $this['sly-current-lang-id'];
+		return $this->get('sly-current-lang-id');
 	}
 
 	/**
 	 * @return sly_Configuration
 	 */
 	public function getConfig() {
-		return $this['sly-config'];
+		return $this->get('sly-config');
 	}
 
 	/**
 	 * @return sly_Event_IDispatcher
 	 */
 	public function getDispatcher() {
-		return $this['sly-dispatcher'];
+		return $this->get('sly-dispatcher');
 	}
 
 	/**
 	 * @return sly_Layout
 	 */
 	public function getLayout() {
-		return $this['sly-layout'];
+		return $this->get('sly-layout');
 	}
 
 	/**
 	 * @return sly_I18N
 	 */
 	public function getI18N() {
-		return $this['sly-i18n'];
+		return $this->get('sly-i18n');
 	}
 
 	/**
 	 * @return sly_Registry_Temp
 	 */
 	public function getTempRegistry() {
-		return $this['sly-registry-temp'];
+		return $this->get('sly-registry-temp');
 	}
 
 	/**
 	 * @return sly_Registry_Persistent
 	 */
 	public function getPersistentRegistry() {
-		return $this['sly-registry-persistent'];
+		return $this->get('sly-registry-persistent');
 	}
 
 	/**
 	 * @return sly_ErrorHandler_Interface
 	 */
 	public function getErrorHandler() {
-		return $this['sly-error-handler'];
+		return $this->get('sly-error-handler');
 	}
 
 	/**
 	 * @return sly_Response
 	 */
 	public function getResponse() {
-		return $this['sly-response'];
+		return $this->get('sly-response');
 	}
 
 	/**
 	 * @return sly_Session
 	 */
 	public function getSession() {
-		return $this['sly-session'];
+		return $this->get('sly-session');
 	}
 
 	/**
 	 * @return sly_DB_PDO_Persistence
 	 */
 	public function getPersistence() {
-		return $this['sly-persistence'];
+		return $this->get('sly-persistence');
 	}
 
 	/**
 	 * @return BabelCache_Interface
 	 */
 	public function getCache() {
-		return $this['sly-cache'];
+		return $this->get('sly-cache');
 	}
 
 	/**
 	 * @return sly_App_Interface
 	 */
 	public function getApplication() {
-		return $this['sly-app'];
+		return $this->get('sly-app');
 	}
 
 	/**
 	 * @return sly_Util_FlashMessage
 	 */
 	public function getFlashMessage() {
-		return $this['sly-flash-message'];
+		return $this->get('sly-flash-message');
 	}
 
 	/**
@@ -239,58 +239,38 @@ class sly_Container implements ArrayAccess, Countable {
 	 * @return sly_Configuration
 	 */
 	protected function buildConfig() {
-		if (!isset($this->values['sly-config'])) {
-			$this['sly-config'] = new sly_Configuration();
-		}
-
-		return $this['sly-config'];
+		return $this['sly-config'] = new sly_Configuration();
 	}
 
 	/**
 	 * @return sly_Event_IDispatcher
 	 */
 	protected function buildDispatcher() {
-		if (!isset($this->values['sly-dispatcher'])) {
-			$this['sly-dispatcher'] = new sly_Event_Dispatcher();
-		}
-
-		return $this['sly-dispatcher'];
+		return $this['sly-dispatcher'] = new sly_Event_Dispatcher();
 	}
 
 	/**
 	 * @return sly_Registry_Temp
 	 */
 	protected function buildTempRegistry() {
-		if (!isset($this->values['sly-registry-temp'])) {
-			$this['sly-registry-temp'] = sly_Registry_Temp::getInstance();
-		}
-
-		return $this['sly-registry-temp'];
+		return $this['sly-registry-temp'] = sly_Registry_Temp::getInstance();
 	}
 
 	/**
 	 * @return sly_Registry_Persistent
 	 */
 	protected function buildPersistentRegistry() {
-		if (!isset($this->values['sly-registry-persistent'])) {
-			$this['sly-registry-persistent'] = sly_Registry_Persistent::getInstance();
-		}
-
-		return $this['sly-registry-persistent'];
+		return $this['sly-registry-persistent'] = sly_Registry_Persistent::getInstance();
 	}
 
 	/**
 	 * @return sly_Response
 	 */
 	protected function buildResponse() {
-		if (!isset($this->values['sly-response'])) {
-			$response = new sly_Response('', 200);
-			$response->setContentType('text/html', 'UTF-8');
+		$response = new sly_Response('', 200);
+		$response->setContentType('text/html', 'UTF-8');
 
-			$this->values['sly-response'] = $response;
-		}
-
-		return $this['sly-response'];
+		return $this->values['sly-response'] = $response;
 	}
 
 	/**
@@ -298,33 +278,21 @@ class sly_Container implements ArrayAccess, Countable {
 	 * @return sly_Session
 	 */
 	protected function buildSession(sly_Container $container) {
-		if (!isset($this->values['sly-session'])) {
-			$this['sly-session'] = new sly_Session($container['config']->get('INSTNAME'));
-		}
-
-		return $this['sly-session'];
+		return $this['sly-session'] = new sly_Session($container->get('sly-config')->get('INSTNAME'));
 	}
 
 	/**
 	 * @return sly_DB_PDO_Persistence
 	 */
 	protected function buildPersistence() {
-		if (!isset($this->values['sly-persistence'])) {
-			$this['sly-persistence'] = sly_DB_Persistence::getInstance();
-		}
-
-		return $this['sly-persistence'];
+		return $this['sly-persistence'] = sly_DB_Persistence::getInstance();
 	}
 
 	/**
 	 * @return BabelCache_Interface
 	 */
 	protected function buildCache() {
-		if (!isset($this->values['sly-cache'])) {
-			$this['sly-cache'] = sly_Cache::factory();
-		}
-
-		return $this['sly-cache'];
+		return $this['sly-cache'] = sly_Cache::factory();
 	}
 
 	/**
@@ -332,18 +300,14 @@ class sly_Container implements ArrayAccess, Countable {
 	 * @return sly_Util_FlashMessage
 	 */
 	protected function buildFlashMessage(sly_Container $container) {
-		if (!isset($this->values['sly-flash-message'])) {
-			sly_Util_Session::start();
+		sly_Util_Session::start();
 
-			$session = $container['session'];
-			$msg     = sly_Util_FlashMessage::readFromSession('sally', $session);
+		$session = $container->get('sly-session');
+		$msg     = sly_Util_FlashMessage::readFromSession('sally', $session);
 
-			$msg->removeFromSession($session);
-			$msg->setAutoStore(true);
+		$msg->removeFromSession($session);
+		$msg->setAutoStore(true);
 
-			$this->values['sly-flash-message'] = $msg;
-		}
-
-		return $this['sly-flash-message'];
+		return $this->values['sly-flash-message'] = $msg;
 	}
 }
