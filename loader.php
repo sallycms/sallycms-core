cf7140cd7f5cb73c4ce7f1b1e42691bde7921485
Christoph <christoph@webvariants.de>
2012-12-20 00:38:31 +0100

    refactored the Composer autoloading once again
    - when we have 5.3, use Composer natively
    - on 5.2, load our Composer wrapper (but still use Composer's data)
    - load and register the old sly_Loader (requires no default load paths,
      as all of them are already handled by Composer)
    - assume the sly_ClassLoader is always present in the bootcache file

--- loader.php
+++ loader.php
@@ -11,29 +11,36 @@
 // load boot cache (frontend or backend, but never when in testing mode)
 $bootcache   = SLY_DYNFOLDER.'/internal/sally/bootcache.php';
 $cacheExists = SLY_IS_TESTING ? false : file_exists($bootcache);
+$isPhp53     = version_compare(PHP_VERSION, '5.3', '>=');
 
 if ($cacheExists) {
 	require_once $bootcache;
 }
 else {
-	require_once SLY_COREFOLDER.'/lib/sly/ClassLoader.php';
+	if (!$isPhp53) {
+		require_once SLY_COREFOLDER.'/lib/sly/ClassLoader.php';
+		require_once SLY_COREFOLDER.'/lib/compatibility.php';
+
+		// this is always required, but loaded by Composer (using a 'require' stmt)
+		require_once SLY_COREFOLDER.'/lib/functions.php';
+	}
+
 	require_once SLY_COREFOLDER.'/lib/sly/Loader.php';
-	require_once SLY_COREFOLDER.'/lib/compatibility.php';
-	require_once SLY_COREFOLDER.'/lib/functions.php';
 }
 
 // the Composer autoloader should be first
-// since Composer still requires __DIR__, this is only used if possible (yet)
-if (version_compare(PHP_VERSION, '5.3', '>=')) {
+if ($isPhp53) {
+	require SLY_VENDORFOLDER.'/autoload.php';
+}
+else {
 	sly_ClassLoader::getLoader(SLY_VENDORFOLDER);
 }
 
+// still load the old one, to give addOns time to update their code base
+// We should remove this once we can properly handle file includes and reach
+// maybe version 0.10 or 0.11.
 sly_Loader::enablePathCache();
-sly_Loader::addLoadPath(SLY_DEVELOPFOLDER.'/lib');
-sly_Loader::addLoadPath(SLY_COREFOLDER.'/lib');
-sly_Loader::addLoadPath(SLY_VENDORFOLDER.'/fabpot/yaml/lib');
-sly_Loader::addLoadPath(SLY_VENDORFOLDER.'/webvariants/babelcache');
 sly_Loader::register();
 
 // cleanup
-unset($bootcache, $cacheExists);
+unset($bootcache, $cacheExists, $isPhp53);
