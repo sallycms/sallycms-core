4dfd24d2363368c01bdec23642aeb78b4abc9b02
Christoph <christoph@webvariants.de>
2013-03-10 08:21:23 +0100

    implement new addListener() method and listener signature in event dispatcher
    --HG--
    branch : event-dispatcher

--- lib/sly/Event/Dispatcher.php
+++ lib/sly/Event/Dispatcher.php
@@ -33,7 +33,7 @@ class sly_Event_Dispatcher implements sly_Event_IDispatcher {
 	}
 
 	/**
-	 * Registers a listener
+	 * Add a listener
 	 *
 	 * Registers a callback for a given event, remembering it for later
 	 * execution. A listener can get a list of special parameters that will be
@@ -42,18 +42,24 @@ class sly_Event_Dispatcher implements sly_Event_IDispatcher {
 	 * @param string  $event     the event name (case sensitive, use upper case by convention)
 	 * @param mixed   $listener  the callback (anything PHP regards as callable)
 	 * @param array   $array     additional params for the listener
-	 * @param boolean $first     if true, the listener will be put in front of existing listeners
+	 * @param boolean $prepend   if true, the listener will be put in front of existing listeners
 	 */
-	public function register($event, $listener, $params = array(), $first = false) {
-		$params = sly_makeArray($params);
-		$item   = array('listener' => $listener, 'params' => $params);
+	public function addListener($event, $listener, array $params = array(), $prepend = false) {
+		return $this->add($event, $listener, $params, $prepend, false);
+	}
 
-		if ($first && !empty($this->listeners[$event])) {
-			array_unshift($this->listeners[$event], $item);
-		}
-		else {
-			$this->listeners[$event][] = $item;
-		}
+	/**
+	 * Registers a listener
+	 *
+	 * @deprecated  since 0.9, use addListener() and the new callback signature instead
+	 *
+	 * @param string  $event     the event name (case sensitive, use upper case by convention)
+	 * @param mixed   $listener  the callback (anything PHP regards as callable)
+	 * @param array   $array     additional params for the listener
+	 * @param boolean $prepend   if true, the listener will be put in front of existing listeners
+	 */
+	public function register($event, $listener, $params = array(), $prepend = false) {
+		return $this->add($event, $listener, sly_makeArray($params), $prepend, true);
 	}
 
 	/**
@@ -63,6 +69,8 @@ class sly_Event_Dispatcher implements sly_Event_IDispatcher {
 	 * @return boolean        true if the event exists, else false
 	 */
 	public function clear($event) {
+		$event = strtoupper($event);
+
 		if (isset($this->listeners[$event])) {
 			$this->listeners[$event] = array();
 			return true;
@@ -90,6 +98,8 @@ class sly_Event_Dispatcher implements sly_Event_IDispatcher {
 	 * @return boolean        true if the event has listeners, else false
 	 */
 	public function hasListeners($event) {
+		$event = strtoupper($event);
+
 		return !empty($this->listeners[$event]);
 	}
 
@@ -100,6 +110,8 @@ class sly_Event_Dispatcher implements sly_Event_IDispatcher {
 	 * @return array          list of listeners (unsorted)
 	 */
 	public function getListeners($event) {
+		$event = strtoupper($event);
+
 		return $this->hasListeners($event) ? $this->listeners[$event] : array();
 	}
 
@@ -115,7 +127,7 @@ class sly_Event_Dispatcher implements sly_Event_IDispatcher {
 	 * @param  array  $params   additional parameters (if necessary)
 	 * @return int              the number of listeners that have been executed
 	 */
-	public function notify($event, $subject = null, $params = array()) {
+	public function notify($event, $subject = null, array $params = array()) {
 		$result = $this->iterate($event, $subject, $params, 'forget');
 		return $result['called'];
 	}
@@ -136,7 +148,7 @@ class sly_Event_Dispatcher implements sly_Event_IDispatcher {
 	 * @return mixed            null if no listeners are set, false if no
 	 *                          listener stops the evaluation or else true
 	 */
-	public function notifyUntil($event, $subject = null, $params = array()) {
+	public function notifyUntil($event, $subject = null, array $params = array()) {
 		$result = $this->iterate($event, $subject, $params, 'stop');
 
 		switch ($result['state']) {
@@ -156,12 +168,12 @@ class sly_Event_Dispatcher implements sly_Event_IDispatcher {
 	 * Listeners cannot stop the evaluation (in contrast to notifyUntil()).
 	 *
 	 * @param  string $event    the event to be triggered
-	 * @param  mixed  $subject  an optional value for the listeners to work with
+	 * @param  mixed  $subject  a start value for the listeners to work with
 	 * @param  array  $params   additional parameters (if necessary)
 	 * @return mixed            the return value of the last listener or the
 	 *                          original subject if no listeners have been set
 	 */
-	public function filter($event, $subject = null, $params = array()) {
+	public function filter($event, $subject, array $params = array()) {
 		$result = $this->iterate($event, $subject, $params, 'filter');
 		return $result['result'];
 	}
@@ -174,32 +186,56 @@ class sly_Event_Dispatcher implements sly_Event_IDispatcher {
 	 * listener should be combined with the next one.
 	 *
 	 * @param  string $event         the event to be triggered
-	 * @param  mixed  $subject       an optional value for the listeners to work with
+	 * @param  mixed  $subject       a value for the listeners to work with
 	 * @param  array  $params        additional parameters (if necessary)
 	 * @param  string $foldStrategy  'filter', 'forget' or 'stop'
 	 * @return array                 an array consisting of 'state' 'called' and 'result'
 	 */
-	protected function iterate($event, $subject, $params, $foldStrategy) {
-		if (!$this->hasListeners($event)) return array('state' => 'empty', 'called' => 0, 'result' => $subject);
+	protected function iterate($event, $subject, array $params, $foldStrategy) {
+		$event = strtoupper($event);
 
-		$params    = sly_makeArray($params);
+		if (!$this->hasListeners($event)) {
+			return array('state' => 'empty', 'called' => 0, 'result' => $subject);
+		}
+
 		$listeners = $this->getListeners($event);
 		$called    = 0;
 
-		$params['event']   = $event;
-		$params['subject'] = $subject;
+		// let listeners know what event they're handling
+		$params['event'] = $event;
 
-		foreach ($listeners as $listener) {
+		foreach ($listeners as $idx => $listener) {
 			$callee = $listener['listener'];
-			$args   = array_merge($params, $listener['params']);
-			$retval = call_user_func($callee, $args);
+			$legacy = $listener['legacy'];
 
+			// skip bad listeners (or else they could break the subject for following listeners)
+			if (!is_callable($callee)) {
+				trigger_error('Listener #'.$idx.' for event '.$event.' is not callable, skipping.', E_USER_WARNING);
+				continue;
+			}
+
+			// merge listener parameters with event paramaters
+			// event parameters take preceedence
+			$args = array_merge($listener['params'], $params);
+
+			// in legacy mode, call $callback($params) with subject in $params
+			if ($legacy) {
+				$args['subject'] = $subject;
+			}
+
+			// in modern mode, call $callback($subject, $params)
+			else {
+				$args = array($subject, $args);
+			}
+
+			// execute the listener
+			$retval = call_user_func($callee, $args);
 			++$called;
 
 			switch ($foldStrategy) {
 				case 'filter':
 					// The return value of this listener shall be the subject of the next one.
-					$params['subject'] = $retval;
+					$subject = $retval;
 					break;
 
 				case 'stop':
@@ -208,6 +244,31 @@ class sly_Event_Dispatcher implements sly_Event_IDispatcher {
 			}
 		}
 
-		return array('state' => 'done', 'result' => $params['subject'], 'called' => $called);
+		return array('state' => 'done', 'result' => $subject, 'called' => $called);
+	}
+
+	/**
+	 * Internal helper to add a listener
+	 *
+	 * This method exists because register() and addListener() do basically the
+	 * same, but we don't want to expose a $legacy parameter in either one of
+	 * them. So this is their common helper.
+	 *
+	 * @param string  $event     the event name (case sensitive, use upper case by convention)
+	 * @param mixed   $listener  the callback (anything PHP regards as callable)
+	 * @param array   $array     additional params for the listener
+	 * @param boolean $prepend   if true, the listener will be put in front of existing listeners
+	 * @param boolean $legacy    true to enable the legacy listener signature, else false
+	 */
+	private function add($event, $listener, array $params, $prepend, $legacy) {
+		$event = strtoupper($event);
+		$item  = array('listener' => $listener, 'params' => $params, 'legacy' => $legacy);
+
+		if ($prepend && !empty($this->listeners[$event])) {
+			array_unshift($this->listeners[$event], $item);
+		}
+		else {
+			$this->listeners[$event][] = $item;
+		}
 	}
 }
