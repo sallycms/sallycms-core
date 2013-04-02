31c2e9a095a3bc6d3ff7d21ccc1062479f9ff53d
Christoph <christoph@webvariants.de>
2013-04-02 04:02:57 +0200

    use the local phpunit installation to not require global PEAR installations anymore

--- tests/setup-travis.sh
+++ tests/setup-travis.sh
@@ -3,6 +3,8 @@
 composer self-update
 composer update --dev
 
+chmod +x vendor/bin/phpunit
+
 mysql -e "CREATE DATABASE sally_test CHARACTER SET utf8 COLLATE utf8_general_ci"
 mysql --database=sally_test < install/mysql.sql
 mysql --database=sally_test -e "INSERT INTO sly_user (id,name,description,login,password,status,attributes,lasttrydate,timezone,createdate,updatedate,createuser,updateuser) VALUES (1, 'Admin', '', 'admin', 'c5e4335577bb89540b15e2f5251e8bc02ced5b32', 1, '{\"isAdmin\":true}', '2011-10-03 04:15:32', 'Europe/Berlin', '2011-04-13 02:37:08', '2011-07-22 02:05:11', 'setup', 'admin')"
