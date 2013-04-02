4f193ba1f05e3ff09c5725a78a2d39bec562128a
Christoph <christoph@webvariants.de>
2013-04-02 04:00:36 +0200

    removed PHP 5.2 cruft

--- tests/setup-travis.sh
+++ tests/setup-travis.sh
@@ -1,30 +1,8 @@
 #!/bin/sh
 
-resetVersion=0
-
-if [ $TRAVIS_PHP_VERSION = '5.2' ]
-then
-  phpenv global 5.3
-  resetVersion=1
-
-  # Make sure to load a composer configuration *without* phpunit, or else we
-  # accidentally overwrite the system-wide classes with our own one (our
-  # autoloader would have preceedence before the system include_path).
-  mv tests/config/travis/composer.json composer.json
-fi
-
 composer self-update
 composer update --dev
 
 mysql -e "CREATE DATABASE sally_test CHARACTER SET utf8 COLLATE utf8_general_ci"
 mysql --database=sally_test < install/mysql.sql
 mysql --database=sally_test -e "INSERT INTO sly_user (id,name,description,login,password,status,attributes,lasttrydate,timezone,createdate,updatedate,createuser,updateuser) VALUES (1, 'Admin', '', 'admin', 'c5e4335577bb89540b15e2f5251e8bc02ced5b32', 1, '{\"isAdmin\":true}', '2011-10-03 04:15:32', 'Europe/Berlin', '2011-04-13 02:37:08', '2011-07-22 02:05:11', 'setup', 'admin')"
-
-if [ $resetVersion -eq 1 ]
-then
-  phpenv global 5.2
-
-  # Manually install DBUnit
-  pear install phpunit/DBUnit
-  phpenv rehash
-fi
