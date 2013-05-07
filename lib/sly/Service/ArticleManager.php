a522c15d7bfe2585eb68de0fb3f9c6cfd2a13079
Zozi <zozi@webvariants.de>
2013-05-07 16:43:25 +0200

    use latest category

--- lib/sly/Service/ArticleManager.php
+++ lib/sly/Service/ArticleManager.php
@@ -52,7 +52,7 @@ abstract class sly_Service_ArticleManager extends sly_Service_ArticleBase {
 		$user        = $this->getActor($user, 'add');
 
 		// get the parent
-		$parentArticle = $this->getArticleService()->findByPK($parentID, $defaultLang);
+		$parentArticle = $this->getArticleService()->findByPK($parentID, $defaultLang, self::FIND_REVISION_LATEST);
 
 		///////////////////////////////////////////////////////////////
 		// check if parent exists
